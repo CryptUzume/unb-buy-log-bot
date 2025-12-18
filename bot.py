d11e42c7926c5a0bb5b5711091edd3375d343c34
CryptUzume <67403313+CryptUzume@users.noreply.github.com>
2025-12-18 20:26:41 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+import os
+import discord
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+client.run(os.environ["DISCORD_TOKEN"])
